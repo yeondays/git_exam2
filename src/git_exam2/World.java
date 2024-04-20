1dc89ac36b3ef7be328e84fad87e30d976f501ff
WNY <na010305@gmail.com>
2024-04-20 13:17:45 +0900

    202114105-4

--- src/git_exam2/World.java
+++ src/git_exam2/World.java
@@ -1,6 +1,13 @@
 package git_exam2;
 
 public class World {
+	static int factorial(int n) {
+		int result = 1;
+		for (int i = 1; i <= n; ++i)
+			result *= i;
+		return result;
+	}
+
 	static int sum(int n) {
 		int result = 0;
 		for (int i = 1; i <= n; ++i)
@@ -11,5 +18,7 @@ public class World {
 	public static void main(String[] args) {
 		for (int i = 5; i <= 10; ++i)
 			System.out.println(sum(i));
+		for (int i = 5; i <= 10; ++i)
+			System.out.println(factorial(i));
 	}
 }
