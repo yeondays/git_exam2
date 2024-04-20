b430204e614cf1cde3fd9ad8ebbdc641c65c9d1e
WNY <na010305@gmail.com>
2024-04-20 13:16:26 +0900

    202114105-3

--- src/git_exam2/Hello.java
+++ src/git_exam2/Hello.java
@@ -2,6 +2,7 @@ package git_exam2;
 
 public class Hello {
 	public static void main(String[] args) {
+		System.out.println("IT융합자율학부");
 		System.out.println("202114105");
 		System.out.println("원나연");
 	}
